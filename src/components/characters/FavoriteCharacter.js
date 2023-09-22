5eb980879923ced0080b0d2e40f51a6bc701a91a
PhiferTristan <phifer.tristan@outlook.com>
2023-09-21 20:54:44 -0500

    got the unfavorite button working

--- src/components/characters/FavoriteCharacter.js
+++ src/components/characters/FavoriteCharacter.js
@@ -1,9 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { deleteFavorite } from "../../services/favoriteService";
 
-export const FavoriteCharacter = ({ character, classes, levels, allUsers }) => {
-  const characterClass = classes.find((cls) => cls.id === character.classId);
-  const characterLevel = levels.find((lvl) => lvl.id === character.levelId);
-  const characterUser = allUsers.find((usr) => usr.id === character.userId);
+export const FavoriteCharacter = ({
+  character,
+  classes,
+  levels,
+  allUsers,
+  favorites,
+  fetchFavoritesByUserId,
+}) => {
+  const characterClass = classes?.find((cls) => cls.id === character.classId);
+  const characterLevel = levels?.find((lvl) => lvl.id === character.levelId);
+  const characterUser = allUsers?.find((usr) => usr.id === character.userId);
+
+  const favorite = favorites.find((fav) => fav.characterId === character.id);
+
+  const navigate = useNavigate();
+
+  const handleUnfavoriteClick = (favoriteId) => {
+  deleteFavorite(favoriteId);
+  navigate(0);
+  console.log(favoriteId);
+  };
 
   return (
     <section key={character.id} className="character">
@@ -16,7 +34,12 @@ export const FavoriteCharacter = ({ character, classes, levels, allUsers }) => {
       </Link>
       <div className="character-class">{characterClass?.class}</div>
       <div className="character-level">Level: {characterLevel?.level}</div>
-      <button className="btn-unfavorite-character">Unfavorite?</button>
+      <button
+        className="btn-unfavorite-character"
+        onClick={() => handleUnfavoriteClick(favorite.id)}
+      >
+        Unfavorite?
+      </button>
       <div className="character-active-status">
         {character.activeStatus ? "Active" : "Not Active"}
       </div>
